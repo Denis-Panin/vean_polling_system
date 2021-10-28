cc5d90909d0702807e45fc9ec3ed19710e376deb
Denis Panin <deniswork2021@ukr.net>
2021-10-28 08:58:04 +0300

    add models, add .gitignore

--- src/polling_system_app/admin.py
+++ src/polling_system_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Survey, Question, ActivePoll, Answer
+
+admin.site.register(Survey)
+admin.site.register(Question)
+admin.site.register(ActivePoll)
+admin.site.register(Answer)
--- src/polling_system_app/models.py
+++ src/polling_system_app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Survey(models.Model):
+    survey_name = models.CharField(max_length=250)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    survey_description = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.survey_name
+
+
+class Question(models.Model):
+    survey = models.ForeignKey(Survey, related_name='questions', on_delete=models.CASCADE)
+    text = models.CharField(max_length=250)
+    type = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.text
+
+
+class ActivePoll(models.Model):
+    question = models.ForeignKey(Question, related_name='active_poll', on_delete=models.CASCADE)
+    active_poll_text = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.active_poll_text
+
+
+class Answer(models.Model):
+    user_id = models.IntegerField()
+    survey = models.ForeignKey(Survey, related_name='survey', on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, related_name='question', on_delete=models.CASCADE)
+    active_poll = models.ForeignKey(ActivePoll, related_name='active_poll', on_delete=models.CASCADE, null=True)
+    active_poll_text = models.CharField(max_length=250, null=True)
+
+    def __str__(self):
+        return self.active_poll_text
